cf16b8921750b282d3e370e5931875870d73be09
Pavel Roslovets <roslovets@exponenta.ru>
2021-03-27 02:08:38 +0300

    livescript2markdown: new API [BREAKING]

--- function/livescript2markdown.m
+++ function/livescript2markdown.m
@@ -1,65 +1,121 @@
-function mdfile = livescript2markdown(filename, options)
+function mdFileAbsPath = livescript2markdown(mlxFilePath, mdFilePath, opts)
 %% Automatically convert Live Script mlx file to markdown file
 arguments
-    filename (1,1) string
-    options.outputfilename char = filename
-    options.format char {mustBeMember(options.format,{'qiita','github'})} = 'github'
-    options.png2jpeg logical = false
-    options.tableMaxWidth (1,1) double = 20
+    mlxFilePath (1,:) char {mustBeFile}
+    mdFilePath (1,:) char = ''
+    opts.Format char {mustBeMember(opts.Format,{'qiita','github'})} = 'github'
+    opts.Png2jpeg logical = false
+    opts.TableMaxWidth (1,1) double = 20
+    opts.FixLinks logical = true % Fix MATLAB special links
+    opts.NormalizeLines logical = true % Remove extra empty lines
+    opts.AddCredentials logical = false % Add toolbox credentials
 end
 
-% Remove extension from file name
-[~, options.outputfilename, ~] = fileparts(options.outputfilename);
-
-% Directory where conversion will be done
-workdir = pwd;
-
-% Add .mlx extension to source file
-if ~endsWith(filename, '.mlx')
-    filename = filename + ".mlx";
-end
-
-% Get mlx file info
-mlxinfo = dir(filename);
-
-% Check that mlx file exists
-if length(mlxinfo) == 0 %#ok<ISMT>
-    error(filename + " does not exist");
+if isempty(mdFilePath)
+    % If no result file specified
+    % Generate file to current folder
+    mdFileFolder = pwd;
+    % Get file name from source file
+    [~, mdFileName, ~] = fileparts(mlxFilePath);
+else
+    % If result file specified
+    [mdFileFolder, mdFileName, mdFileExt] = fileparts(mdFilePath);
+    % Check result file extension
+    if mdFileExt ~= ".md"
+        error('Markdown file should have .md extension');
+    end
+    % Select result file folder
+    if isempty(mdFileFolder)
+        mdFileFolder = pwd;
+    end
 end
 
 % Get full mlx file path
-mlxfile = fullfile(mlxinfo.folder, mlxinfo.name);
+mlxFileAbsPath = getAbsPath(mlxFilePath);
+
+% Directory where conversion will be done
+workFolder = getAbsPath(mdFileFolder);
 
 % Get latex file path
-latexfile = fullfile(workdir, options.outputfilename + ".tex");
+latexFileAbsPath = fullfile(workFolder, mdFileName + ".tex");
 
 % Create list of temporary files that will be deleted after conversion
-tempfiles = [latexfile; "matlab.sty"];
+tempFiles = [latexFileAbsPath; fullfile(workFolder, "matlab.sty")];
 % Delete files only if they don't exist before conversion
-todelete = ~isfile(tempfiles);
-filestodelete = tempfiles(todelete);
+filesToDelete = tempFiles(~isfile(tempFiles));
 
 % Get path of images directory
-imgdir = fullfile(workdir, options.outputfilename + "_images");
+imgFolder = fullfile(workFolder, mdFileName + "_images");
 % Check that images directory doesn't exist before conversion
-noimgdir = ~isfolder(imgdir);
+isNoImgFolder = ~isfolder(imgFolder);
 
 % Convert mlx to latex (WARNING: undocumented function)
-matlab.internal.liveeditor.openAndConvert(char(mlxfile), char(latexfile));
+matlab.internal.liveeditor.openAndConvert(char(mlxFileAbsPath), char(latexFileAbsPath));
 
-% Convert latex t markdown
-args = namedargs2cell(options);
-mdfile = latex2markdown(latexfile, args{:});
-mdfile = fullfile(workdir, mdfile);
+% Go to new work folder
+prevWorkFolder = cd(workFolder);
+
+% Prepare latex to markdown conversion parameters
+argOpts = struct('outputfilename', mdFileName, 'format', opts.Format, ...
+    'png2jpeg', opts.Png2jpeg, 'tableMaxWidth', opts.TableMaxWidth);
+args = namedargs2cell(argOpts);
+% Call latex2markdown function (convert latex to markdown)
+mdFileNameExt = latex2markdown(latexFileAbsPath, args{:});
+% Convert result to absolute path
+mdFileAbsPath = getAbsPath(mdFileNameExt);
+
+% Return to previous work folder
+cd(prevWorkFolder);
 
 % Delete temporary files
-for i = 1 : length(filestodelete)
-    if isfile(filestodelete(i))
-        delete(filestodelete(i));
+for i = 1 : length(filesToDelete)
+    if isfile(filesToDelete(i))
+        delete(filesToDelete(i));
     end
 end
 
 % Delete empty images folder
-if noimgdir && isfolder(imgdir) && length(dir(imgdir)) == 2
-    rmdir(imgdir);
+if isNoImgFolder && isfolder(imgFolder) && length(dir(imgFolder)) == 2
+    rmdir(imgFolder);
+end
+
+%% Markdown file postprocessing
+% Read text from markdown file
+txt = fileread(mdFileAbsPath);
+txt = strtrim(txt);
+% Process text
+if opts.FixLinks
+    txt = replace(txt, "[matlab:" + ["open" "winopen" "cd"] + " ", '[');
+end
+if opts.NormalizeLines
+    ls = split(string(txt), newline);
+    ni = find(ls == "");
+    nid = [0; diff(ni)];
+    ls(ni(nid == 1)) = [];
+    txt = join(ls, newline);
+    txt = strtrim(txt);
+end
+if opts.AddCredentials
+    [~, mlxFileName, mlxFileExt] = fileparts(mlxFilePath);
+    txt = string(txt) + newline + newline +...
+        sprintf("***\n*Generated from %s with [Live Script to Markdown Converter](%s)*",...
+        string(mlxFileName) + mlxFileExt, 'https://github.com/roslovets/livescript2markdown');
+end
+% Write processed text back to file
+writematrix(char(txt), mdFileAbsPath, 'FileType', 'text', 'QuoteStrings', false);
+end
+
+
+function absPath = getAbsPath(relPath)
+%% Convert relative path to absolute
+arguments
+    relPath (1,:) char
+end
+if ~isfile(relPath) && ~isfolder(relPath)
+    error('No such file or folder: %s', relPath);
+end
+% Get file/folder info
+info = dir(relPath);
+% Get absolute path
+absPath = fullfile(info(1).folder, info(1).name);
 end
